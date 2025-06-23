2e8c9390ef9ec6749abd89b9365a21a2bc2abe6f
Lucas Marques Mendes <lucasmqmendes@gmail.com>
2025-06-22 21:13:42 -0300

    Efeito Knockback

--- platform-2d-godot-4.0/scripts/player.gd
+++ platform-2d-godot-4.0/scripts/player.gd
@@ -5,6 +5,9 @@ const SPEED = 200.0
 const JUMP_FORCE = -400.0
 
 var is_jumping := false
+@export var player_life := 10
+var knockback_vector := Vector2.ZERO
+
 @onready var animation := $Anime as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
 
@@ -34,13 +37,45 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("Idle")
 
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
+		
+		
 	move_and_slide()
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemies"):
+	#if body.is_in_group("enemies"):
+		#queue_free()
+	if player_life < 0:
 		queue_free()
+	else:
+		if $ray_right.is_colliding():
+			take_damage(Vector2(-200, -200))
+		elif $ray_left.is_colliding():
+			take_damage(Vector2(200, -200))
 		
 func follow_camera(camera):
 	var camera_path = camera.get_path()
 	remote_transform.remote_path = camera_path
+
+func take_damage(Knockback_force := Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+	
+	if Knockback_force != Vector2.ZERO:
+		knockback_vector = Knockback_force
+		
+		var knockback_tween := get_tree().create_tween().parallel()
+		knockback_tween.tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1,0,0,1)
+		knockback_tween.tween_property(animation,"modulate", Color(1,1,1,1), duration)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
